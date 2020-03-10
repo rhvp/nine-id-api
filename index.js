cfd86c4ea7b51fb58b9f72212dab2eb8e4c19ba2
Abdul Ajoge <bugsabdul@yahoo.com>
2020-03-10 15:26:50 +0100

    unhandled Rejection and Process handling

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ require('dotenv').config();
 
 const app = require('./app');
 const mongoose = require('./config/mongoose');
-
+process.on('uncaughtException', err => {
+    console.log('Uncaught Exception!! Shutting down process..', err.name, err.message, err.stack);
+    process.exit(1);
+})
 const port = process.env.PORT || 5000;
 
 app.listen(port,()=>{
     console.log('App running on Port:', port)
 });
+
+
+process.on('unhandledRejection', err=>{
+    console.log('Unhandled Rejection!!', err.name, err.message, err.stack);
+})
